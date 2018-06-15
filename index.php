e379367b4a41ea72cffe7eb64a219f6263f66293
Anderson <anderson.ti@plasutil.com.br>
2018-06-15 20:00:39 -0300

    Realizado o CRUD do admin

--- index.php
+++ index.php
@@ -44,6 +44,7 @@ $app->get('/admin/login', function(){
     $page->setTpl("login");
 });
 
+// Valida o login e redireciona
 $app->post('/admin/login', function(){
 
     User::login($_POST["login"], $_POST["password"]);
@@ -59,6 +60,107 @@ $app->get('/admin/logout', function(){
     User::logout();
 
     header("Location: /admin");
+    exit;
+
+});
+
+// Listar usuarios
+$app->get('/admin/users', function(){
+
+    User::verifyLogin();
+
+    $users = User::listAll();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("users", array(
+        "users"=>$users
+    ));
+
+});
+
+// Cadastrar usuario
+$app->get('/admin/users/create', function(){
+
+    User::verifyLogin();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("users-create");
+
+});
+
+// Gravando usuário
+$app->post("/admin/users/create", function(){
+
+    User::verifyLogin();
+
+    $user = new User();
+
+    $_POST["inadmin"] = (isset($_POST["inadmin"]))? 1 : 0;
+    $_POST["despassword"] = password_hash($_POST["despassword"], PASSWORD_DEFAULT, [
+        "cost"=>12
+    ]);
+
+    $user->setData($_POST);
+
+    $user->save();
+
+    header("Location: /admin/users");
+
+    exit;
+
+
+});
+
+// Excluindo o usuario
+$app->get("/admin/users/:iduser/delete", function($iduser){
+
+    User::verifyLogin();
+
+    $user = new User();
+
+    $user->get((int)$iduser);
+
+    $user->delete();
+
+    header("Location: /admin/users");
+    exit;
+});
+
+// Alterar usuario
+$app->get('/admin/users/:iduser', function($iduser){
+
+    User::verifyLogin();
+
+    $user = new User();
+
+    $user->get((int)$iduser);
+
+    $page = new PageAdmin();
+
+    $page->setTpl("users-update", array(
+        "user"=>$user->getValues()
+    ));
+
+});
+
+// Alterando o usuário
+$app->post("/admin/users/:iduser", function($iduser){
+
+    User::verifyLogin();
+
+    $_POST["inadmin"] = (isset($_POST["inadmin"]))? 1 : 0;
+
+    $user = new User();
+
+    $user->get((int)$iduser);
+
+    $user->setData($_POST);
+
+    $user->update();
+
+    header("Location: /admin/users");
     exit;
 
 });
