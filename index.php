79cd3d15e63170b7ce26e5e852987b8e31c6ea7b
Anderson <anderson.ti@plasutil.com.br>
2018-06-16 15:04:22 -0300

    Recuperar senha

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 session_start();
 
+require_once("res/config.php");
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
@@ -165,6 +166,81 @@ $app->post("/admin/users/:iduser", function($iduser){
 
 });
 
+// Esqueceu a senha
+$app->get("/admin/forgot", function(){
+
+    $page = new PageAdmin(array(
+        "header"=>false,
+        "footer"=>false
+    ));
+
+    $page->setTpl("forgot");
+});
+
+// Enviando link de recuperação
+$app->post("/admin/forgot", function(){
+
+    $user = User::getForgot($_POST['email']);
+
+    header("Location: /admin/forgot/sent");
+    exit;
+});
+
+// Confirmação de envio do email redefinição de senha
+$app->get("/admin/forgot/sent", function(){
+
+    $page = new PageAdmin(array(
+        "header"=>false,
+        "footer"=>false
+    ));
+
+    $page->setTpl("forgot-sent");
+});
+
+
+// Informar a nova senha
+$app->get("/admin/forgot/reset", function(){
+
+    $user = User::validForgotDecrypt($_GET["code"]);
+
+    $page = new PageAdmin(array(
+        "header"=>false,
+        "footer"=>false
+    ));
+
+    $page->setTpl("forgot-reset", array(
+        "name"=>$user["desperson"],
+        "code"=>$_GET["code"]
+    ));
+});
+
+// Gravando a nova senha
+$app->post("/admin/forgot/reset", function(){
+
+    $forgot = User::validForgotDecrypt($_POST["code"]);
+
+    User::setForgotUsed($forgot["idrecovery"]);
+
+    $user = new User();
+
+    $user->get($forgot["iduser"]);
+
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT, [
+        "cost"=>12
+    ]);
+
+    $user->setPassword($password);
+
+    $page = new PageAdmin(array(
+        "header"=>false,
+        "footer"=>false
+    ));
+
+    $page->setTpl("forgot-reset-success");
+
+});
+
+
 $app->run();
 
- ?>
+?>
---
+++ res/config.php
@@ -0,0 +1,6 @@
+<?php
+
+$baseUrl = "http://".$_SERVER['HTTP_HOST'];
+$baseUrl .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+
+define("BASE_URL", $baseUrl);
