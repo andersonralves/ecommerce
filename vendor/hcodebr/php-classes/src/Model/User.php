4567b61fa1b28ee87659ee5d57b8ab2f19e7d1d2
Anderson <anderson.ti@plasutil.com.br>
2018-06-25 16:21:59 -0300

    Minha Conta

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -12,6 +12,7 @@ class User extends Model
     const SECRET = "HcodePhp7_Secret";
     const ERROR = "UserError";
     const ERROR_REGISTER = "UserErrorRegister";
+    const SUCCESS = "UserSuccess";
 
     public static function getFromSession()
     {
@@ -332,6 +333,25 @@ class User extends Model
     public static function clearErrorRegister()
     {
         $_SESSION[User::ERROR_REGISTER] = NULL;
+    }
+
+    public static function setSuccess($msg)
+    {
+        $_SESSION[USER::SUCCESS] = $msg;
+    }
+
+    public static function getSuccess()
+    {
+        $msg = (isset($_SESSION[User::SUCCESS]) && $_SESSION[User::SUCCESS]) ?  $_SESSION[User::SUCCESS] : "";
+
+        User::clearSuccess();
+
+        return $msg;
+    }
+
+    public static function clearSuccess()
+    {
+        $_SESSION[User::SUCCESS] = NULL;
     }
 
     public static function checkLoginExist($login)
